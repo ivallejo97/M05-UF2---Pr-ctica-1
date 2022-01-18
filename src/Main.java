6936e621c1f9dbb268cb68c498ed6c7d2e3b928b
ivallejo97 <ivallejo@elpuig.xeill.net>
2022-01-18 13:07:55 +0100

    Programa1-V2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Wordl hsa");
         System.out.println("Hello Wordl 2109");
+        System.out.println("ddggfbv");
     }
 }
